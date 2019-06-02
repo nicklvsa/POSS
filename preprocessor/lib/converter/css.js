7b889f398f15262e9e998e3a29572a99536b1a37
Nick Gerancher <nick@nicksdesk.com>
2019-06-01 23:23:34 -0400

    starting to add preprocessor

---
+++ preprocessor/lib/converter/css.js
@@ -0,0 +1,34 @@
+var POSS_SPECIAL_FUNCTIONS = [
+	'@respond', //one argument(_responder), opens as function
+	'@inherit', //one modifier<class/id> and one argument(_class/id)
+	'@calc'//three arguments(num1, num2, 'expr')
+];
+
+var POSS_SPECIAL_KEYWORDS = [
+	'var',
+	'as',
+
+	'boolean',
+	'string',
+	'number',
+	'array',
+
+	'phone',
+	'tablet',
+	'desktop',
+
+	'redef',
+	'mod',
+
+	'class',
+	'id'
+];
+
+module.exports = (css) => {
+	return new Converter(css);
+}
+
+function Converter(css) {
+	//convert poss -> css
+
+}
---
+++ preprocessor/lib/poss-lib.js
@@ -0,0 +1,1 @@
+exports.convertCSS = require('./converter/css');
